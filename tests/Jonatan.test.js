599c52327de7d829c34b0fa4b84890e3e0796d09
jona-terceros <jonatan.terceros@ucb.edu.bo>
2023-10-17 12:48:38 -0400

    refactoring tests

--- tests/Jonatan.test.js
+++ tests/Jonatan.test.js
@@ -2,52 +2,49 @@ import FuncionesWordle from "../src/funcionesWordle";
 
 
 describe('FuncionesWordle', () => {
+    let funcionesWordle;  
+
+    beforeEach(() => {
+      funcionesWordle = new FuncionesWordle();  
+    });
     it('debería devolver la lista de palabras seleccionables', () => {
-      // Arrange (preparar)
-      const funcionesWordle = new FuncionesWordle();
-      // Act (actuar)
       const resultado = funcionesWordle.obtenerListaPalabras();
-      // Assert (verificar)
       expect(resultado).toEqual([
         "HOJA", "HOLA", "LOBO", "COLA", "PALO", "PICO", "COCA", "PERO", "DUKE",
         "RAYOS", "MESSI", "PALOS", "LIBRO", "LLAVE", "BALON", "CINCO"
       ]);
     });
     it('debería devolver la lista de las categorias de Deportes', () => {
-      const funcionesWordle = new FuncionesWordle();
       const resultado = funcionesWordle.obtenerCategoriaDeporte();
       expect(resultado).toEqual(["MESSI", "BALON", "DUKE", "CINCO"]);
     });
     it('debería devolver la lista de las categorias de Sistemas', () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       const resultado = funcionesWordle.obtenerCategoriaSistemas();
       expect(resultado).toEqual(["HOLA", "PERO"]);
     });
     it('deberia concatenar correctamente la cadena de resultado', () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       expect(funcionesWordle.cadenaResultado).toBeUndefined();
       funcionesWordle.cadenaResultado = ["H", "O", "L", "A"];
       funcionesWordle.concatenarResultado();
       expect(funcionesWordle.cadenaResultado).toEqual("HOLA");
     });
     it('debería agregar la pista correctamente', () => {
-      const funcionesWordle = new FuncionesWordle();
-
+      
       funcionesWordle.nroIntentos = 3; 
       funcionesWordle.cadenaResultado = 'LOBO'; 
-
       funcionesWordle.agregarPista();
-
       expect(funcionesWordle.listaPistas[2]).toEqual('LOBO');
     });
     it('deberia mostrar un mensaje de error: Ya existe esa palabra.', () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       expect(() => {
         funcionesWordle.definirNuevaPalabra('LLAVE');
       }).toThrowError('Ya existe esa palabra.');
     });
     it('deberia agregar la nueva palabra al array de palabras seleccionables', () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       expect(() => {
         funcionesWordle.definirNuevaPalabra('CINE');
       }).not.toThrow();
@@ -56,45 +53,42 @@ describe('FuncionesWordle', () => {
     });
 
     it('deberia manejar la excepción y retornar el mensaje correcto al agregar una palabra existente', () => {
-      const funcionesWordle = new FuncionesWordle();
+     
       const resultado = funcionesWordle.agregarNuevaPalabra('HOJA');
       expect(resultado).toBe('Ya existe esa palabra.');
       expect(funcionesWordle.palabrasSeleccionables).toContain('HOJA');
     });
 
     it('deberia agregar correctamente una palabra nueva y retornar la palabra', () => {
-      const funcionesWordle = new FuncionesWordle();
+    
       const resultado = funcionesWordle.agregarNuevaPalabra('NUEVA');
       expect(resultado).toBe('NUEVA');
       expect(funcionesWordle.palabrasSeleccionables).toContain('NUEVA');
     });
 
     it("no deberia marcarse como amarilla si no está en la palabra secreta ", () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       funcionesWordle.cadenaResultado = ['X', 'X', 'X', 'X', 'X', 'X'];
       funcionesWordle.copiaPalabraSecreta = ['H', 'O', 'J', 'A'];
-  
       funcionesWordle.definirAmarillas(['H', 'O', 'J', 'A'], ['H', 'X', 'X', 'A']);
       expect(funcionesWordle.cadenaResultado).toEqual(['a', 'X', 'X', 'a', 'X', 'X']);
     });
     it("deberia devolver la misma cadena resultado", () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       funcionesWordle.cadenaResultado = ['X', 'X', 'X', 'X', 'X', 'X'];
       funcionesWordle.copiaPalabraSecreta = ['H', 'O', 'J', 'A'];
       funcionesWordle.definirAmarillas(['H', 'O', 'J', 'A'], ['Y', 'Z', 'W', 'X']);
       expect(funcionesWordle.cadenaResultado).toEqual(['X', 'X', 'X', 'X', 'X', 'X']);
     });
     it("deberia devolver la misma cadena resultado y se mantiene sin cambios", () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       funcionesWordle.cadenaResultado = ['z', 'z', 'z', 'z', 'z', 'z'];
       funcionesWordle.copiaPalabraSecreta = ['H', 'O', 'J', 'A'];
-    
       funcionesWordle.definirAmarillas(['H', 'O', 'J', 'A'], ['X', 'Y', 'W', 'X']);
-    
       expect(funcionesWordle.cadenaResultado).toEqual(['z', 'z', 'z', 'z', 'z', 'z']);
     });
     it("deberia devolver la mismca cadena resultado sin entradas", () => {
-      const funcionesWordle = new FuncionesWordle();
+     
       funcionesWordle.cadenaResultado = ['X', 'X', 'X', 'X', 'X', 'X'];
       funcionesWordle.copiaPalabraSecreta = ['H', 'O', 'J', 'A'];
       funcionesWordle.definirAmarillas([], []);
@@ -102,25 +96,22 @@ describe('FuncionesWordle', () => {
     });
 
     it("deberia devolver una palabra al azar del array de categoria deportes", () => {
-      const funcionesWordle = new FuncionesWordle();
+      
       const palabraSec = funcionesWordle.definirPalabraSecretaAlAzar("Deporte");
-     
       expect(funcionesWordle.categoriaDeporte).toContain(palabraSec);
     });
     it("deberia devolver una palabra al azar del array de categoria UCB", () => {
-      const funcionesWordle = new FuncionesWordle();
-      const palabraSec = funcionesWordle.definirPalabraSecretaAlAzar("UCB");
      
+      const palabraSec = funcionesWordle.definirPalabraSecretaAlAzar("UCB");
       expect(funcionesWordle.categoriaUCB).toContain(palabraSec);
     });
     it("deberia devolver una palabra al azar del array de categoria Sistemas", () => {
-      const funcionesWordle = new FuncionesWordle();
+
       const palabraSec = funcionesWordle.definirPalabraSecretaAlAzar("Sistemas");
-     
       expect(funcionesWordle.categoriaSistemas).toContain(palabraSec);
     });
     it("deberia devolver una palabra al azar del array de categoria que no existe", () => {
-      const funcionesWordle = new FuncionesWordle();
+
       const palabraSec = funcionesWordle.definirPalabraSecretaAlAzar("CualquierCategoriaQueNoExiste");
       expect(funcionesWordle.palabrasSeleccionables).toContain(palabraSec);
     });
