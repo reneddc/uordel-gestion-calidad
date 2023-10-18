9b164c688e82ade6d8a973c56db0a723b45bb9c9
Adriana Tarquino <atarquinocespedes@gmail.com>
2023-10-18 01:05:51 -0400

    unit_test: isIncompleteAttempt()

--- tests/adry.test.js
+++ tests/adry.test.js
@@ -1,23 +1,46 @@
 import FuncionesWordle from "../src/funcionesWordle";
 
+import WordleError from "../src/ExcepcionesWordle";
 
-describe('generarResultadoVacio', () => {
+describe("generarResultadoVacio", () => {
     let funcionesWordle;
-  
+
     beforeEach(() => {
-      funcionesWordle = new FuncionesWordle();
+        funcionesWordle = new FuncionesWordle();
     });
-  
-    it('debería generar un resultado vacío con la longitud correcta', () => {
-      const tamPalabraSecreta = 5; // Establece la longitud deseada
-  
-      // Llama al método generarResultadoVacio con la longitud deseada
-      funcionesWordle.generarResultadoVacio(tamPalabraSecreta);
-  
-      // Verifica que la propiedad cadenaResultado se haya generado correctamente
-      expect(funcionesWordle.cadenaResultado).toHaveLength(tamPalabraSecreta);
-      expect(funcionesWordle.cadenaResultado.every(letra => letra === 'o')).toBe(true);
+    /*------------------------------1.generarResultadoVacio ----------------------------------*/
+
+    it("debería generar un resultado vacío con la longitud correcta", () => {
+        const tamPalabraSecreta = 5; // Establece la longitud deseada
+
+        funcionesWordle.generarResultadoVacio(tamPalabraSecreta);
+
+        expect(funcionesWordle.cadenaResultado).toHaveLength(tamPalabraSecreta);
+        expect(
+            funcionesWordle.cadenaResultado.every((letra) => letra === "o")
+        ).toBe(true);
+    });
+
+    /*--------------------------------2.esIntentoIncompleto--------------------------------*/
+
+    it("debería lanzar una excepción WordleError cuando la palabra es incompleta", () => {
+        const intento = "hola";
+        const tamPalabraSecreta = 6;
+
+        // Utiliza una aserción para verificar que se lance una excepción WordleError con el mensaje correcto
+        expect(() =>
+            funcionesWordle.esIntentoIncompleto(intento, tamPalabraSecreta)
+        ).toThrowError(WordleError, "Palabra Incompleta");
+    });
+
+    it("no debería lanzar una excepción cuando la palabra es completa", () => {
+        const intento = "palabra";
+        const tamPalabraSecreta = 7;
+        // No se espera que se lance una excepción
+        expect(() =>
+            funcionesWordle.esIntentoIncompleto(intento, tamPalabraSecreta)
+        ).not.toThrow();
     });
 
     
-  });
+});
